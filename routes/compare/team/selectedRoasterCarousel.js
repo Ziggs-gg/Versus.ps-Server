c35e2e235a73532c4c7e2a59ee59633f793a8f39
WONSIK <omsoms777@naver.com>
2022-06-06 21:56:46 +0900

    Modify SelectedTeamRoasterCarousel 팀이미지경로 추가

--- routes/compare/team/selectedRoasterCarousel.js
+++ routes/compare/team/selectedRoasterCarousel.js
@@ -8,18 +8,20 @@ router.get('/', async (req, res) => {
 	let ptID = req.query.ptID;
 	const SelectedTeamRoasterCarousel =
 		`
-		SELECT phID, player, ptID, role, ROUND(((kills + assists) / deaths), 2) AS KDA, DPM, GPM, VSPM, imgPath
+		SELECT phID, player, ptID, role, ROUND(((kills + assists) / deaths), 2) AS KDA, DPM, GPM, VSPM, playerIMGPath, teamIMGpath
 	FROM
 	(SELECT gs.phID, SUBSTRING_INDEX(gs.phID, '-', -1) AS player, SUBSTRING_INDEX(gs.phID, '-', 4) AS ptID,
 		role, SUM(kills) AS kills, SUM(deaths) AS deaths, SUM(assists) AS assists, 
-		ROUND(AVG(DPM), 1) AS DPM, ROUND(AVG(GPM), 1) AS GPM, ROUND(AVG(VSPM), 2) AS VSPM, imgPath
+		ROUND(AVG(DPM), 1) AS DPM, ROUND(AVG(GPM), 1) AS GPM, ROUND(AVG(VSPM), 2) AS VSPM, playerIMGPath, teamIMGPath
 		FROM games_stats AS gs
 	INNER JOIN
-		(SELECT phID, imgPath FROM playerIMGpath) AS pI
+		(SELECT phID, imgPath AS playerIMGpath FROM playerIMGpath) AS pI
         ON gs.phID = pI.phID
+        INNER JOIN (SELECT ptID, imgPath AS teamIMGpath FROM teamIMGpath) AS tI
+        ON SUBSTRING_INDEX(gs.phID, '-', 4) = tI.ptID
 	GROUP BY gs.phID, role
     ) AS toCal
-WHERE ptID = '${ptID}'
+WHERE ptID = '${ptID}' -- 파라미터 영역
 ORDER BY 
 	FIELD (role, 'TOP', 'JUNGLE', 'MID', 'ADC', 'SUPPORT');
 	`;
